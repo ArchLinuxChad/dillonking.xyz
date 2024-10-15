daec601c488993e76088894d4b7c549da831f729
Codex08 <dillonking2008@gmail.com>
2024-10-15 08:57:10 +0100

    updated

--- src/vars.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-page_title = "Dillon King"
-
-image = "/home/dillon/gitlab-repos/dillonking.xyz/assets/dillon.jpg"
-
-css = "/home/dillon/gitlab-repos/dillonking.xyz/style/styles.css"
---
+++ vars.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+page_title = "Dillon King"
+
+css = "style/styles.css"
