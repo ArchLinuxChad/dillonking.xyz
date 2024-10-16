cc2bd2bf686183d643fe6bedf6125c511d88b0b6
ArchLinuxChad <thedillonking@proton.me>
2024-10-16 10:57:18 +0100

    updated

--- projects.py
+++ projects.py
@@ -5,6 +5,6 @@ def main():
     with st.container():
         st.header("Grade Reviewer")
         st.write("Visualises GSCE grades into charts to make it easier to understand.")
-        st.write("Still in Creation")
+        st.link_button("Check it out here", "https://gradesanalyser.streamlit.app")
 
     st.write("---")
